b53ccac85845c10132ddc3e0809f7100d1dba23c
Aravindan22 <63189666+Aravindan22@users.noreply.github.com>
2023-02-11 11:57:47 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-apt-get install aria2
+apt-get install -y aria2
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-!tar -xvzf ngrok-v3-stable-linux-amd64.tgz
+tar -xvzf ngrok-v3-stable-linux-amd64.tgz
